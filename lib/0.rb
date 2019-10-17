7823f086f2866db7fbb1980723504a3211c806bb
Sacha TORDJMAN <sacha.tordjman@gmail.com>
2019-10-17 14:32:08 +0200

    Création de l'architecture vide

---
+++ lib/0.rb
@@ -0,0 +1,9 @@
+#require 'rubygems'
+#-------Pour le scrapping-------
+#require 'nokogiri'
+#require 'open-uri'
+#-------Pour simuler un navigateur-------
+#require 'watir'
+#require 'launchy'
+#-------Pour les tests------
+#require 'pry'
